f3461b243422290c5a7996490fce658e37e5ebf7
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-05 12:09:25 -0800

    Add employee questionnaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+print "What is your name?\n"
+	employee_name = gets.chomp
+print "How old are you?\n"
+	employee_age = gets.chomp
+print "What year were you born?\n"
+	employee_birth_year = gets.chomp
+print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)\n"
+	order_bread = gets.chomp
+print "Would you like to enroll in the company's health insurance? (y/n)\n"
+	insurance = gets.chomp
+
+puts employee_name
+puts employee_age
+puts employee_birth_year
+puts order_bread
+puts insurance
