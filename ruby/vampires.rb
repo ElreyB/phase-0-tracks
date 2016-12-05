26ced0d37d03b3679d757a56025ab1ed52174c4f
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-05 12:35:06 -0800

    Add condition to confirm age

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,23 @@
 print "What is your name?\n"
 	employee_name = gets.chomp
 print "How old are you?\n"
-	employee_age = gets.chomp
+	employee_age = gets.chomp.to_i
 print "What year were you born?\n"
-	employee_birth_year = gets.chomp
+	employee_birth_year = gets.chomp.to_i
+
+if 2016 - employee_birth_year == employee_age
+  age = true
+elsif 2016 - employee_birth_year == employee_age + 1
+  age = true
+else
+  age = false 
+end
+
+	
 print "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)\n"
 	order_bread = gets.chomp
 print "Would you like to enroll in the company's health insurance? (y/n)\n"
 	insurance = gets.chomp
 
-puts employee_name
-puts employee_age
-puts employee_birth_year
-puts order_bread
-puts insurance
+
+
