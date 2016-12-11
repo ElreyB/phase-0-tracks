1217e3e301995c2c83e4ab8833793b7737dce6e1
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-11 08:22:53 -0800

    Add pseudocode for program

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+# write a program that will allow an interior designer to enter the details of a 
+# given client: the client's name, phone number, email,do the have kids, if so ,
+# number of children, decor theme, 
+#Convert input to the appropriate data type
+
+
+#print hash
+
+#Prompt user which data they wish to change
+	# if the user writes 'none' skip
+	#but if user wants to change data
+		#update data
+#print updated hash
+
