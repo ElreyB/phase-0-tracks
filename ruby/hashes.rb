6b6cf36f1e08a6b03ee57c24613934868d766983
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-11 08:33:20 -0800

    Add prompt for name

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,7 +1,10 @@
 # write a program that will allow an interior designer to enter the details of a 
-# given client: the client's name, phone number, email,do the have kids, if so ,
+# given client: the client's name, phone number, email,do they have kids, if so ,
 # number of children, decor theme, 
 #Convert input to the appropriate data type
+puts "Please anwser the following questions:"
+puts "What is you full name?"
+	full_name = gets.chomp
 
 
 #print hash
