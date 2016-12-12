d4e9fc0cb4f6c2a85676143456e82e223f24943a
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-12 14:45:59 -0800

    Add classroom level key with values

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -24,6 +24,12 @@ dance_school = {
 	},
 	jazz_class: {
 		styles: ['Tap Dacne', 'Swing', 'Flossie'],
+		classroom_level: {
+			room1: 'Level 1',
+			room2: 'Level 2',
+			room3: 'Level 3',
+			room4: 'Level 4'
+		},
 	}
 		
 }
