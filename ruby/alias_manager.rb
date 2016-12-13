2902dc6dca32199ef13e9c5f1667dbe2d901558f
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-13 09:03:39 -0800

    Add next vowel method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,20 @@
 #Write a method that takes a name  and creates a fake name by 
 
+#Methods to
 #Swapping the first and last name.
 def swap(full_name)
 	split_name = full_name.split(' ')
 	swapped_name = split_name.reverse.join(' ')
 	swapped_name	
 end
+#Driver code
+#p swap("Elrey Belmonti")
 
-p swap("Elrey Belmonti")
 #Change vowels to the next vowel.
+def next_vowel(string)
+	string.tr!("aeiouAEIOU", "eiouaEIOUA")
+	string
+end
 
+p next_vowel("ElreyA BelmOnti")
 #Change consonants to the next consonant.
