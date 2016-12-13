0ddd436940f8e59541a57063cbbb0a708a008c5f
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-13 08:51:18 -0800

    Add method to swap names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,13 @@
 #Write a method that takes a name  and creates a fake name by 
 
 #Swapping the first and last name.
+def swap(full_name)
+	split_name = full_name.split(' ')
+	swapped_name = split_name.reverse.join(' ')
+	swapped_name	
+end
 
+p swap("Elrey Belmonti")
 #Change vowels to the next vowel.
 
 #Change consonants to the next consonant.
