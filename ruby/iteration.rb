9d146702ffa218db8bb93b48fa3c7406e6eb6624
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-13 14:24:02 -0800

    Add array

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -2,5 +2,16 @@ def foo(array)
 	array.each {|item| puts "This is an item in the array: #{item}"}
 end
 array = ["a","b","c","d"]
-
 p foo(array)
+
+colors = ["red", "green", "blue"]
+
+profile = {
+	name: "Bob",
+	age: 55,
+	hair_color: "Purple",
+	shoe_size: 10,
+	happy_person: true
+}
+
+p profile
