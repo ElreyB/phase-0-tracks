fb1160cc61b39208d77be4a7c32682e03439adf1
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-13 08:36:14 -0800

    Add pseudocode for methods

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,7 @@
+#Write a method that takes a name  and creates a fake name by 
+
+#Swapping the first and last name.
+
+#Change vowels to the next vowel.
+
+#Change consonants to the next consonant.
