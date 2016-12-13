dac2ceb79fe6fe3257cc4c0946092e6ada1b9cfb
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-13 11:53:57 -0800

    Add method to print names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -31,13 +31,20 @@ def fakename(name)
 end
 #p fakename("Felicia Torres")
 
+#Method to print names
+def print_list(hash_list)
+	hash_list.each do |name, alias_name|
+		puts "#{name} your alias is #{alias_name}"
+	end 
+end
 
 #user Interface
 print "\t***Alias Creater***\n"
 print "Enter first and last name to create an alias.\n"
 print "Press 'Enter' and create another alias.\n"
 print "Type 'quit' when done.(Case sensitive)\n"
-print "Enter first and last names to create an alias:\n"
+puts "------------------------------------"
+print "Enter first and last names to create aliases:\n"
 agent_name = ''
 stored_names = {}
 	until agent_name == "quit"
@@ -50,4 +57,6 @@ stored_names = {}
 		end
 		stored_names[agent_name]= alias_name
 	end
-puts alias_name
+puts "------------------------------------"
+print_list(stored_names)
+#End of program
