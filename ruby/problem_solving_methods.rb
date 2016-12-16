ec4fa12ba94d57fbb8acaa01590f30398e4db5f7
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-16 14:25:56 -0800

    Add logic to add number to array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -5,26 +5,26 @@
 #The method should return the index of the item, or nil if the integer 
 #is not present in the array. Don't use built-in array methods like
 
-def search_array(array, item)
-	counter = 0
-	array.each do |element|	
-		if item == element
-			 counter
-			  break
-		else
-			 counter += 1
-		end
-	end
-	#If item is not in the array return nil
-	if counter >= array.length
-			return nil
-		else
-			counter
-		end			
-end
+# def search_array(array, item)
+# 	counter = 0
+# 	array.each do |element|	
+# 		if item == element
+# 			 counter
+# 			  break
+# 		else
+# 			 counter += 1
+# 		end
+# 	end
+# 	#If item is not in the array return nil
+# 	if counter >= array.length
+# 			return nil
+# 		else
+# 			counter
+# 		end			
+# end
 
-arr = [42, 89, 23, 1] 
-p search_array(arr, 1)
+# arr = [42, 89, 23, 1] 
+# p search_array(arr, 1)
 #=> 3
 
 #Method that does the Fibonacci terms
@@ -33,8 +33,14 @@ p search_array(arr, 1)
 #Lenght of the array is the same as the given number
 #Return the array on sums
 def fib(number)
-	
+	arr = [0,1]
+	counter = 2
+		arr[counter] = arr[counter-2] + arr[counter-1]
+		 arr
 end
+
+p fib(6)
+#-->[0,1,1,2,3,5]
 
 
 
