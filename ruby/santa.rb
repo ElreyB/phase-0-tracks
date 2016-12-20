d5da746d2131ec492b6358125aaa48f5ada59047
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-20 08:27:44 -0800

    Add cookies instance method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,9 +8,14 @@ class Santa
 	def speak
 		print "Ho, ho, ho! Haaaapppy holidays!"
 	end
+
+	def eat_milk_and_cookies(cookie)
+		print "That was a good #{cookie}!"
+	end
 end
 
 santa = Santa.new
 
 p santa
-p santa.speak
+puts santa.speak
+puts santa.eat_milk_and_cookies("sugar cookie")
