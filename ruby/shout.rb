54c61826539093462be33b29376c7a682f77d132
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-20 13:52:08 -0800

    Add shout mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,23 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + "😢 "
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!" + "😃 "
+# 	end
+# end
+
+# p Shout.yell_angrily("Why is learn Ruby so hard")
+# p Shout.yell_happily("Because learning is half the battle")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + "😢 "
 	end
 
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!" + "😃 "
 	end
 end
 
-p Shout.yell_angrily("Why is learn Ruby so hard")
-p Shout.yell_happily("Because learning is half the battle")
