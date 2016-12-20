895ced4351d00547540ef028671c4b1995b74c82
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-20 09:30:49 -0800

    Addcelebrate bday instance method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,7 +5,7 @@ class Santa
 		@gender = gender
 		@ethnicity = ethnicity
 		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0	
+		@age = 0	
 	end
 
 	def speak
@@ -15,10 +15,21 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		print "That was a good #{cookie}!"
 	end
+
+	def celebrate_birthday
+		@age += 1
+	end
 end
 
-santa = Santa.new
+# santa = Santa.new
 
-p santa
-puts santa.speak
-puts santa.eat_milk_and_cookies("sugar cookie")
+# p santa
+# puts santa.speak
+# puts santa.eat_milk_and_cookies("sugar cookie")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   p santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
