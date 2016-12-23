2d5b2182e6a82c26fb6f2349982ae7556b27fbac
Elrey Belmonti <elbelmonti@gmail.com>
2016-12-23 09:00:30 -0800

    Add test for storing word

--- ruby/Sologame/word_guess_game_spec.rb
+++ ruby/Sologame/word_guess_game_spec.rb
@@ -1,2 +1,8 @@
-require_relative 'WordGuess'
+require_relative 'wordguess'
+	describe WordGuess do
+		let(:word) { WordGuess.new("unicorn") }
 
+	it "stores the word given on initialization" do 
+		expect(wordguess.get_word).to eq "unicorn"
+	end
+end
