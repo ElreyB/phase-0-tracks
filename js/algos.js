0e95b9d85453938af894cc1a9c24fd70f14acd04
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-04 10:59:46 -0800

    Add pseudocode longest word function

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Function: that takes an array of words or phrases 
+// 			and returns the longest word or phrase in the array
+// input: an array of words or phrases
+// steps: have an temp variable to hold the longest word
+// 		  have a longest word value to compare to
+// 		  build a loop that will go through the elements in the array
+// 			if the word is longer than the longest word value
+					// the longest word value to compare to becomes the new compare value
+					// the temp variable value is that word
+// output: the longest word
