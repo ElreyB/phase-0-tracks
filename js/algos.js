a917a6879d359a71cbc35bc4aeb82468c51ea651
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-04 11:17:08 -0800

    Add longestWord function

--- js/algos.js
+++ js/algos.js
@@ -8,3 +8,30 @@
 					// the longest word value to compare to becomes the new compare value
 					// the temp variable value is that word
 // output: the longest word
+
+var phrases = ["long phrase","longest phrase","longer phrase"];
+var names = ["Elrey", "Bob", "Bill Clinton", "Jack Daniels", "Cindy Smith"];
+function longestWord(list) {
+	valueToCompare = 0;
+	word = "";
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].length > valueToCompare) {
+			valueToCompare = list[i].length;
+			word = list[i]
+		}
+	} 
+	return word;
+}
+// check length of phrases
+for (var i = 0; i < phrases.length; i++) {
+	console.log(phrases[i].length);
+}
+// driver code
+console.log(longestWord(phrases));
+console.log("--------------------");
+// check length of names
+for (var i = 0; i < names.length; i++) {
+	console.log(names[i].length);
+}
+// driver code
+console.log(longestWord(names));
