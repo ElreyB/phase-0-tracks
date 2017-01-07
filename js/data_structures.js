b5052cffde81bc7b7d40d29bb6fe19c21264f93a
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-06 16:49:44 -0800

    Add constructor function driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -31,3 +31,12 @@ function Car(model, year, color) {
 
 var newCar = new Car("Ford", 2015, "Red");
 console.log(newCar);
+newCar.rev();
+console.log("-----------");
+var newCar2 = new Car("Prius", 2017, "Silver");
+console.log(newCar2);
+newCar.rev();
+console.log("-----------");
+var newCar3 = new Car("Jaguar", 1980, "Cherry");
+console.log(newCar3);
+newCar.rev();
