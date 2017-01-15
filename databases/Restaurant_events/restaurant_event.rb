790fbc39de56ac7b00ff8bdcba9f0533949d1be1
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-14 18:58:43 -0800

    Add pseudocode to restaurant_event file

---
+++ databases/Restaurant_events/restaurant_event.rb
@@ -0,0 +1,46 @@
+# Restaurant Events
+
+# require gems if needed
+
+# create SQLite3 database
+
+# create schema tables with fancy string delimiters
+
+# Business code
+
+# Method: Adds guest to guest table
+# input: full name ----> string
+# steps: inserts name into guests table
+# output: N/A
+
+# Method: Adds occasion to occasions table
+# input: type of occasion ------> string
+# steps: inserts occasion type into occasions table
+# output: N/A
+
+# Method: Adds event type to types table
+# input: event type ------->string
+# steps: inserts event type into types table
+# output: N/A
+
+# Method: Adds event information to events table
+# input: guest_id ---> integer, occasion_id ---> integer, type_id ---> integer
+			   # datetime ---> string 
+# steps: inserts information into events table
+# output: N/A
+
+
+
+# Interface
+# Introduce event program
+# Ask how many events they wish to add to database
+# loop until done
+# Ask for guests full name
+# Ask for guest's event occasion
+# Ask for dat and time of event
+# Ask if they was to see full list of events or just one event
+	# Show events in a user friendly way
+	# or
+	# Show one event
+# End program
+# Driver code
