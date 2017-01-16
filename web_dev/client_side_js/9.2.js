c364d1deb12bb54cc4035ad152321e0206e71de7
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-16 14:35:00 -0800

    Add javascript function manipulation

--- web_dev/client_side_js/9.2.js
+++ web_dev/client_side_js/9.2.js
@@ -1,10 +1,27 @@
-console.log("The script is running!");
+// console.log("The script is running!");
 
-function addPinkBorder(event) {
-  console.log("click happened! here's the click event:");
-  console.log(event);
-  event.target.style.border = "2px solid pink";
+// function addPinkBorder(event) {
+//   console.log("click happened! here's the click event:");
+//   console.log(event);
+//   event.target.style.border = "2px solid pink";
+// }
+
+// var photo = document.getElementById("lizard-photo");
+// photo.addEventListener("click", addPinkBorder);
+
+
+
+function addBackgroundColor() {
+	var photo = document.getElementById("lizard-photo");
+	photo.style.padding = "3px";
+	photo.style.backgroundColor = "green";
+}
+addBackgroundColor();
+
+function addFontChange() {
+	var tag = document.getElementsByTagName("a");
+	var grumpy = tag[0];
+	grumpy.style.fontSize = "3em";
 }
 
-var photo = document.getElementById("lizard-photo");
-photo.addEventListener("click", addPinkBorder);
+addFontChange();
