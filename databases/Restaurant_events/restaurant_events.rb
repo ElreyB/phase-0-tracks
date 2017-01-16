ab4457fedea04679f64b02fe01847cb46469a15e
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-15 18:15:58 -0800

    Add control flow for entering guest data

--- databases/Restaurant_events/restaurant_events.rb
+++ databases/Restaurant_events/restaurant_events.rb
@@ -4,6 +4,7 @@
 require 'sqlite3'
 # create SQLite3 database
 db = SQLite3::Database.new("restaurant_events.db")
+db.results_as_hash = true
 
 # create schema tables with fancy string delimiters
 create_table_guests = <<-SQL
@@ -87,9 +88,11 @@ end
 # Introduce event program
 puts "Welcome to your Events Database Program Setup tutorial."
 puts "Let's get start........"
+puts "Let us setup your database for the types of occasions you have at you establishment."
 puts "Enter the type of occasions you have at you establishment one at a time." 
 puts "Example: Birthday, Anniversary, etc."
 puts "Type 'done' when finish."
+puts "\n"
 puts "What type of occasions do you have at your establishment?"
 	occasion_input = ""
 	establish_occasions = []
@@ -107,6 +110,18 @@ puts "What type of occasions do you have at your establishment?"
 		add_occasion(db, occasion)
 	end
 
+puts "Here is how the occasions database looks like..."
+puts "================================================="
+puts "Occasions"
+puts "-----------"
+occasion_list = db.execute("SELECT * FROM occasions")
+occasion_list.each do |occasion|
+	puts "#{occasion['id']}" + " | " + "#{occasion['name']}"
+end
+puts "Notice that every entry you make with have a unique number assign to it."
+puts "Don't worry about this now but know you will use this unique number later."
+puts "\n"
+puts "Now, let us setup your database for event type you have at you establishment."
 puts "Enter the event type you have at you establishment one at a time." 
 puts "Example: Private, Semi_private, etc."
 puts "Type 'done' when finish."
@@ -125,6 +140,78 @@ puts "What event type do you have at your establishment?"
 	establish_types.each do |type|
 		add_type(db, type)
 	end
+puts "Okay, now that that is done let us enter your establishment's guests' information."
+puts "The format for this is:"
+puts "Guest full name, email, and contact number in this order."
+puts "If they do not have an email and/or contact number type 'null' as a place holder."
+puts "In a later tutorial we learn how to update your database."
+puts "For this tutorial let's enter everything that is needed for your database."
+puts "The program will first ask you how many guests you are entering."
+puts "You can enter 1-100 guests if you like but for now let's do 3."
+puts "It will then ask you for a full name, an email, and a contact number individually."
+puts "Example: Full name: 'Bob Smith', Email: bobisgreat@aol.com, Contact number : 1234567890"
+puts "Let's give it a try."
+puts "\n"
+puts "How many guests will you enter today?"
+	amount_of_guests = gets.chomp.to_i 
+	establish_guests = []
+	until amount_of_guests == 0
+		puts "What is the guest's full name?"
+		print "Name: \n"
+		full_name = gets.chomp.to_s
+		puts "What is the guest's email?"
+		print "Email: \n"
+		email = gets.chomp.to_s
+		puts "What is the guest's contact number?"
+		print "Contact number: \n"
+		contact_number = gets.chomp.to_i
+		guests_list = [add_guest(db, full_name, email, contact_number)]
+		establish_guests.push(guests_list)
+	amount_of_guests -= 1
+	end
+puts "\n"
+puts "Now it is time to combine the data to make a compelet events database."
+puts "Remember those unique ids from before:"
+occasion_list = db.execute("SELECT * FROM occasions")
+occasion_list.each do |occasion|
+	puts "#{occasion['id']}" + " | " + "#{occasion['name']}"
+end
+puts "We will be entering those ids in our events database allow with a date and time of event."
+puts "It will look wired are first but in later tutorials this part will have more of 'user friendly' look it."
+puts "For now, we will just setup your events database in a raw way."
+puts "\n"
+puts "The program will first ask you how many events you are entering. Lets ust do 2 for now."
+puts "The program will then ask you for 4 things."
+puts "Guest id, Occasion id, Event id, Date and time of the event which is done in militray time."
+puts "Example: Guest id: 1,Occasion id: 3, Event id: 2, Date and time: 11-22-2016 15:00"
+puts "Let give it try."
+puts "\n"
+puts "How many events are you entering?"
+	amount_of_events = gets.chomp.to_i
+	establish_events = []
+	until amount_of_events == 0
+		puts "What is the guest's id?"
+		print "Guest id: \n"
+		guest_id = gets.chomp.to_i
+		puts "What is the occasion id?"
+		print "Occasion id: \n"
+		occasion_id = gets.chomp.to_i
+		puts "What is the event id?"
+		print "Event id: \n"
+		event_id = gets.chomp.to_i
+		puts "What is the date and time of event?"
+		print "Date and time: \n"
+		date_time = gets.chomp.to_s
+		events_list = [add_event(db, guest_id, occasion_id, event_id, date_time)]
+		establish_events.push(events_list)
+	amount_of_events -= 1
+	end
+
+
+
+
+
+
 
 # Ask how many events they wish to add to database
 # loop until done
