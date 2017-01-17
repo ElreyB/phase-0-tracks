a14b8dd17826428d532379254004278ae7fe61f4
Elrey Belmonti <elbelmonti@gmail.com>
2017-01-17 15:55:56 -0800

    Add jquery syntax

--- web_dev/jquery/client_side_js/9.2.js
+++ web_dev/jquery/client_side_js/9.2.js
@@ -1,46 +1,29 @@
-// 9.2 Ally and Elrey
+// function addBackgroundColor() {
+// 	var photo = document.getElementById("lizard-photo");
+// 	photo.style.padding = "3px";
+// 	photo.style.backgroundColor = "green";
+// }
+// addBackgroundColor();
+
+$(function(){
+	$("#lizard-photo").css({
+		"background-color": "green", 
+		"padding": "3px" 
+	});
+});
+
+// function addFontChange() {
+// 	var tag = document.getElementsByTagName("a");
+// 	var grumpy = tag[0];
+// 	grumpy.style.fontSize = "3em";
+// }
+// addFontChange();
 
 // console.log("The script is running!");
-
-// function addPinkBorder(event) {
-//   console.log("click happened! here's the click event:");
-//   console.log(event);
-//   event.target.style.border = "2px solid pink";
-// }
-
-// var photo = document.getElementById("lizard-photo");
-// photo.addEventListener("click", addPinkBorder);
+$(function(){
+	$("a").css({"font-size":"3em"});
+});
 
 
-
-function addBackgroundColor() {
-	var photo = document.getElementById("lizard-photo");
-	photo.style.padding = "3px";
-	photo.style.backgroundColor = "green";
-}
-addBackgroundColor();
-
-function addFontChange() {
-	var tag = document.getElementsByTagName("a");
-	var grumpy = tag[0];
-	grumpy.style.fontSize = "3em";
-}
-addFontChange();
-
-console.log("The script is running!");
-
-function addHover(event) {
-	console.log("click happened.")
-	console.log(event)
-	event.target.style.fontStyle = "italic";
-}
-var great = document.getElementById("strong");
-
-great.addEventListener("mouseover", addHover);
-
-function addMouseOut(event) {
-	event.target.style.fontStyle = "normal";
-}
-great.addEventListener("mouseout", addMouseOut);
 
 
